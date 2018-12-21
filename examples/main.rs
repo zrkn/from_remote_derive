05a4bac47cf028864e57575e4d10c6e5a8112502
zrkn <zrkn@email.su>
2018-12-21 17:17:45 +0300

    Add support for multiple derives

--- examples/main.rs
+++ examples/main.rs
@@ -3,7 +3,7 @@ use std::collections::{HashMap, BTreeMap};
 use from_remote_derive::FromRemote;
 
 #[derive(FromRemote)]
-#[from_remote = "Bar"]
+#[from_remote("Bar", "Pook")]
 struct Foo {
     bar: BTreeMap<u64, u64>,
     fizz: Vec<String>,
@@ -14,8 +14,13 @@ struct Bar {
     fizz: Vec<String>,
 }
 
+struct Pook {
+    bar: BTreeMap<u64, u64>,
+    fizz: Vec<String>,
+}
+
 #[derive(FromRemote)]
-#[from_remote = "Buzz"]
+#[from_remote("Buzz")]
 enum Fizz {
     A(u64, Option<u64>),
     B(HashMap<String, Foo>),
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate proc_macro;
 use syn::{
     parse_macro_input, parse_quote,
     DeriveInput, Attribute, Type, Meta, Lit, Data, Fields, Ident, Field,
+    NestedMeta,
     spanned::Spanned,
 };
 use quote::{quote, quote_spanned};
@@ -13,12 +14,22 @@ use proc_macro2::TokenStream;
 pub fn derive_struct_from(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
-    let name = input.ident;
-    let remote_name = get_remote_name_from_attrs(&input.attrs);
+    let impls = get_remote_name_from_attrs(&input.attrs)
+        .into_iter()
+        .map(|remote_name| from_impl(&input, remote_name));
 
-    let constructor_impl = match input.data {
+    let expanded = quote! {
+        #(#impls)*
+    };
+
+    proc_macro::TokenStream::from(expanded)
+}
+
+fn from_impl(input: &DeriveInput, remote_name: Type) -> TokenStream {
+    let name = &input.ident;
+    let constructor_impl = match &input.data {
         Data::Struct(data) => {
-            match data.fields {
+            match &data.fields {
                 Fields::Named(fields) => {
                     let fields_match = named_match(fields.named.iter());
                     let fields_mapping = named_mapping(fields.named.iter());
@@ -84,15 +95,13 @@ pub fn derive_struct_from(input: proc_macro::TokenStream) -> proc_macro::TokenSt
         _ => panic!("Only structs and enums are supported by #[derive(FromRemote)]"),
     };
 
-    let expanded = quote! {
+    quote! {
         impl From<#remote_name> for #name {
             fn from(other: #remote_name) -> Self {
                 #constructor_impl
             }
         }
-    };
-
-    proc_macro::TokenStream::from(expanded)
+    }
 }
 
 fn named_match<'a>(fields: impl Iterator<Item = &'a Field>) -> TokenStream {
@@ -222,7 +231,7 @@ fn is_monadic(field: &Field) -> bool {
     monadic_idents.contains(ident)
 }
 
-fn get_remote_name_from_attrs(attrs: &[Attribute]) -> Type {
+fn get_remote_name_from_attrs(attrs: &[Attribute]) -> Vec<Type> {
     for attr in attrs {
         let path = match attr.path.segments.first() {
             Some(p) => p,
@@ -233,14 +242,21 @@ fn get_remote_name_from_attrs(attrs: &[Attribute]) -> Type {
             continue
         }
 
-        let meta = match attr.parse_meta().unwrap() {
-            Meta::NameValue(m) => m,
+        let list = match attr.parse_meta().unwrap() {
+            Meta::List(l) => l,
             _ => continue,
         };
 
-        if let Lit::Str(lit) = meta.lit {
-            return lit.parse::<Type>().unwrap();
-        }
+        return list.nested.iter()
+            .filter_map(|meta| {
+                match meta {
+                    NestedMeta::Literal(Lit::Str(lit)) => {
+                        Some(lit.parse::<Type>().unwrap())
+                    }
+                    _ => None
+                }
+            })
+            .collect();
     }
     panic!("#[derive(FromRemote)] must be used with #[from_remote = \"???\"]")
 }
